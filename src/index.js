2f739b038edb443848e4a3b6ce97b01fe5f675ac
joao <joao.pedro.patrocinio@ubi.pt>
2025-10-22 07:46:32 +0100

    XOPS-15-atualizar-team-contract

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'Hello from Sprint 133' });
+  res.json({ msg: 'Hello from Sprint 1' });
 });
 
 module.exports = app;
