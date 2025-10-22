664aed8a1ec7ee1d98f5eb57e8891a92a4000c9e
joao <joao.pedro.patrocinio@ubi.pt>
2025-10-22 07:21:39 +0100

    XOPS-13 Atualizar endpoint Hello World

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'Hello from Sprint 13' });
+  res.json({ msg: 'Hello from Sprint 133' });
 });
 
 module.exports = app;
