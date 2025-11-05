cfdb265fa40eaa9bd9f4490a0cc27a4e3ae66bf5
joao <joao.pedro.patrocinio@ubi.pt>
2025-11-05 09:13:58 +0000

    XOPS-18: Testes automatizados concluídos para endpoint /users

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Hello from Sprint 13' });
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    { id: 1, name: 'João Patrocínio' },
+    { id: 2, name: 'Micaela' },
+    { id: 3, name: 'Miguel' }
+  ]);
+});
+
 module.exports = app;
